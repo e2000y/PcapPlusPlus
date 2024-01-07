bcbae52694e38f2a2a980c6b025acbc9dff64d67
Eric Yung <eric.yung@jspectrum.com>
2024-01-07 14:51:37 +0000

    include JDK path in cmake files;

--- javaCpp_ex/src/Dpdk_IPv4.cpp
+++ javaCpp_ex/src/Dpdk_IPv4.cpp
@@ -1,7 +1,10 @@
 #define LOG_MODULE JavaCPPLogModuleDPDKIPv4
 
 #include <time.h>
+#include <ctime>
 #include <stdexcept>
+#include <exception>
+#include <jni.h>
 #include "Logger.h"
 #include "RawPacket.h"
 #include "Packet.h"
@@ -17,24 +20,38 @@ namespace pcpp
 class AppWorkerThread : public DpdkWorkerThread
 {
 private:
+    JavaVM* m_jvm;
+    JNIEnv* m_env;
     DpdkDevice* m_dpdkDev;
+    uint16_t m_queue;
     IPReassembly* m_reassembly;
+    Dpdk_Dev_Rx_Stats* m_stat;
     uint32_t m_mBufPoolSize;
-    void (*m_callback)(bool isEnd, long long time, IPv4Layer* layer);
+    std::string m_cbClz;
+    std::string m_cbMtd;
+    std::string m_cbSig;
     bool m_stop = false;
     uint32_t m_coreId;
     
 public:
-    AppWorkerThread(uint32_t mBufPoolSize, DpdkDevice* dpdkDev, IPReassembly* reassembly, void (*callback)(bool isEnd, long long time, IPv4Layer* layer))
+    AppWorkerThread(void* ptr, uint32_t mBufPoolSize, DpdkDevice* dpdkDev, uint16_t queue, IPReassembly* reassembly, Dpdk_Dev_Rx_Stats* stat, const std::string& cbClz, const std::string& cbMtd, const std::string& cbSig)
     {
+        m_jvm = (JavaVM*) ptr;
         m_dpdkDev = dpdkDev;
+        m_queue = queue;
         m_reassembly = reassembly;
         m_mBufPoolSize = mBufPoolSize;
-        m_callback = callback;
+        m_cbClz = cbClz;
+        m_cbMtd = cbMtd;
+        m_cbSig = cbSig;
+
+        PCPP_LOG_INFO("AppWorkerThread assigned for DPDK device - " << dpdkDev->getDeviceName() << ", queue: " << queue);
     }
 
     ~AppWorkerThread()
-    { }
+    {
+        m_jvm->DetachCurrentThread();
+    }
 
     void stop()
     {
@@ -59,69 +76,98 @@ public:
 
             ret = false;
         }
+        else if (m_jvm->AttachCurrentThread((void **)&m_env, NULL) < 0)
+        {
+            PCPP_LOG_ERROR("Cannot attach JVM for core - " << coreId);
+
+            ret = false;
+        }
         else
         {
             PCPP_LOG_INFO("DPDK device " << m_dpdkDev->getDeviceId() << ":" << m_dpdkDev->getDeviceName() << " use core ID " << coreId);
 
-            const uint16_t queues = m_dpdkDev->getNumOfOpenedRxQueues();
             MBufRawPacket* packetArr[MAX_RECEIVE_BURST] = {};
+            //  for stats reporting
+            int iter = 0;
+            std::time_t last = std::time(nullptr);
+            DpdkDevice::DpdkDeviceStats theStat;
 
             while (!m_stop)
             {
-                uint16_t errors = 0;
-
-                for (uint16_t q = 0; q < queues; q++)
+                //  we do every 100 iterations - saving CPU
+                if ((iter % 100) == 0)
                 {
-                    uint16_t packetsReceived = m_dpdkDev->receivePackets(packetArr, MAX_RECEIVE_BURST, q);
+                    std::time_t now = std::time(nullptr);
 
-                    if (packetsReceived == 0)
+                    int diff = (int) now - last;
+
+                    //  only report at most 1 minute interval
+                    if (diff >= 60)
                     {
-                        PCPP_LOG_ERROR("Cannot get packet from " << m_dpdkDev->getDeviceId() << ":" << m_dpdkDev->getDeviceName() << ", queue: " << q);
+                        last = now;
 
-                        errors++;
+                        m_dpdkDev->getStatistics(theStat);
+
+                        m_stat->tv_sec = (long long) theStat.timestamp.tv_sec;
+                        m_stat->rxDrop = theStat.rxPacketsDroppedByHW;
+                        m_stat->rxErrs = theStat.rxErroneousPackets;
+                        m_stat->rxMbufAllocFail = theStat.rxMbufAlocFailed;
+                        m_stat->packets = theStat.aggregatedRxStats.packets;
+                        m_stat->bytes = theStat.aggregatedRxStats.bytes;
+                        m_stat->packetsPerSec = theStat.aggregatedRxStats.packetsPerSec;
+                        m_stat->bytesPerSec = theStat.aggregatedRxStats.bytesPerSec;
                     }
-                    else
+
+                    iter = 0;
+                }
+
+                uint16_t packetsReceived = m_dpdkDev->receivePackets(packetArr, MAX_RECEIVE_BURST, m_queue);
+
+                for (int i = 0; i < packetsReceived; i++)
+                {
+                    //  parse packet
+                    Packet* pkt = getIPv4Layer(packetArr[i], m_reassembly);
+
+                    if (pkt != nullptr)
                     {
-                        for (int i = 0; i < packetsReceived; i++)
+                        IPv4Layer* ipLayer = pkt->getLayerOfType<pcpp::IPv4Layer>(true);
+
+                        if (ipLayer != nullptr)
                         {
-                            //  parse packet
-                            Packet* pkt = getIPv4Layer(packetArr[i], m_reassembly);
+                            timespec t = packetArr[i]->getPacketTimeStamp();
 
-                            if (pkt != nullptr)
+                            long long time = (t.tv_sec * 1000L) + (t.tv_nsec / 1000000L);
+
+                            try
                             {
-                                IPv4Layer* ipLayer = pkt->getLayerOfType<pcpp::IPv4Layer>(true);
-
-                                if (ipLayer != nullptr)
-                                {
-                                    timespec t = packetArr[i]->getPacketTimeStamp();
-
-                                    long long time = (t.tv_sec * 1000L) + (t.tv_nsec / 1000000L);
-
-                                    m_callback(false, time, ipLayer);
-                                }
-
-                                delete pkt;
+                                //m_callback(false, time, ipLayer);
                             }
+                            catch (...)
+                            {
+                                std::exception_ptr ex = std::current_exception();
 
-                            packetArr[i]->clear();
+                                try
+                                {
+                                    std::rethrow_exception(ex);
+                                }
+                                catch (const std::exception& e)
+                                {
+                                    PCPP_LOG_ERROR("Got exception from JVM - " << e.what());
+                                }
+                            }
                         }
+
+                        delete pkt;
                     }
+
+                    packetArr[i]->clear();
                 }
 
-                if (errors == queues)
-                {
-                    m_stop = true;
-
-                    PCPP_LOG_ERROR("Cannot get packet for all queues from " << m_dpdkDev->getDeviceId() << ":" << m_dpdkDev->getDeviceName() << ", break");
-
-                    ret = false;
-                }
+                iter++;
             }
         }
 
         PCPP_LOG_INFO("DPDK device " << m_dpdkDev->getDeviceId() << ":" << m_dpdkDev->getDeviceName() << " processing loop end");
-
-        m_callback(true, 0L, nullptr);
 
         return ret;
     }
@@ -131,16 +177,15 @@ Dpdk_Ipv4::Dpdk_Ipv4(const std::string& app, const std::vector<std::string>& arg
 	m_reassembly(nullptr, nullptr, maxIPReassembly)
 {
     m_coreMask = createCoreMaskFromCoreIds(svcCores);
-
     m_mBufPoolSizePerDevice = mBufPoolSizePerDevice;
 
     createCoreVectorFromCoreMask(m_coreMask, m_coresToUse);
 
     if (m_coresToUse.size() < 1)
     {
-        PCPP_LOG_ERROR("Needed minimum of 1 core");
+        PCPP_LOG_ERROR("Needed minimum of 1 service core");
 
-        throw new std::out_of_range("Needed minimum of 1 core");
+        throw new std::out_of_range("Needed minimum of 1 service core");
     }
 
     //  convert string array to **char
@@ -157,18 +202,21 @@ Dpdk_Ipv4::Dpdk_Ipv4(const std::string& app, const std::vector<std::string>& arg
         cstrings.push_back(vstrings.back().data());
     }
 
-    if (!DpdkDeviceList::getInstance().initDpdk(m_coreMask, mBufPoolSizePerDevice, masterCore, cstrings.size(), cstrings.data(), app))
-    {
-        PCPP_LOG_ERROR("Couldn't initialize DPDK");
+    //  for DPDK init
+    std::vector<int> nSvcCores = svcCores;
+    nSvcCores.push_back((int) masterCore);
 
-        throw new std::runtime_error("Couldn't initialize DPDK");
+    CoreMask coreMask = createCoreMaskFromCoreIds(nSvcCores);
+
+    if (!DpdkDeviceList::initDpdk(coreMask, mBufPoolSizePerDevice, masterCore, cstrings.size(), cstrings.data(), app))
+    {
+        PCPP_LOG_ERROR("Couldn't initialize DPDK library");
+
+        throw new std::runtime_error("Couldn't initialize DPDK library");
     }
-
-    if ((m_coreMask & DpdkDeviceList::getInstance().getDpdkMasterCore().Mask) != 0)
+    else
     {
-        PCPP_LOG_ERROR("coreMask cannot include master core");
-
-        throw new std::range_error("coreMask cannot include master core");
+        PCPP_LOG_INFO("DPDK library inited");
     }
 
     const std::vector<DpdkDevice*> devs = DpdkDeviceList::getInstance().getDpdkDeviceList();
@@ -193,11 +241,11 @@ Dpdk_Ipv4::~Dpdk_Ipv4()
     DpdkDeviceList::getInstance().stopDpdkWorkerThreads();
 }
 
-bool Dpdk_Ipv4::startProcess(const std::vector<std::string> devs, void (*callback)(bool isEnd, long long time, IPv4Layer* layer))
+bool Dpdk_Ipv4::startProcess(void* ptr, const std::vector<std::string> devs, const uint16_t queues, const std::string& cbClz, const std::string& cbMtd, const std::string& cbSig)
 {
     if (m_coresToUse.size() < devs.size())
     {
-        PCPP_LOG_ERROR("Needed at least " << devs.size() << " cores to process the given devices");
+        PCPP_LOG_ERROR("Needed at least " << devs.size() << " service cores to process the given device list");
 
         return false;
     }
@@ -206,15 +254,47 @@ bool Dpdk_Ipv4::startProcess(const std::vector<std::string> devs, void (*callbac
         const std::vector<DpdkDevice*> dpdkDevs = DpdkDeviceList::getInstance().getDpdkDeviceList();
         std::vector<DpdkWorkerThread *> workerThreadsVec;
 
-        for (DpdkDevice* dpdkDev : dpdkDevs)
+        for (std::string dev : devs)
         {
-            if (std::find(std::begin(devs), std::end(devs), dpdkDev->getPciAddress()) != std::end(devs))
+            DpdkDevice* dpdkDev = DpdkDeviceList::getInstance().getDeviceByPciAddress(dev);
+
+            if (dpdkDev != nullptr)
             {
-                workerThreadsVec.push_back(new AppWorkerThread(m_mBufPoolSizePerDevice, dpdkDev, &m_reassembly, callback));
+                dpdkDev->clearStatistics();
+
+                bool ok = true;
+
+                //  open device with # of queues
+                if (!dpdkDev->isOpened())
+                {
+                    ok = dpdkDev->openMultiQueues(queues, 0);
+                }
+
+                DpdkDevice::LinkStatus linkSts;
+
+                dpdkDev->getLinkStatus(linkSts);
+
+                if (ok)
+                {
+                    PCPP_LOG_INFO("Found DPDK device - " << dpdkDev->getDeviceName() << ", link UP: " << linkSts.linkUp << ", Speed: " << linkSts.linkSpeedMbps << ", Duplex: " << linkSts.linkDuplex);
+
+                    Dpdk_Dev_Rx_Stats* devStat = new Dpdk_Dev_Rx_Stats();
+
+                    stats[dpdkDev->getPciAddress()] = devStat;
+
+                    for (uint16_t q = 0; q < dpdkDev->getNumOfOpenedRxQueues(); q++)
+                    {
+                        workerThreadsVec.push_back(new AppWorkerThread(ptr, m_mBufPoolSizePerDevice, dpdkDev, q, &m_reassembly, devStat, cbClz, cbMtd, cbSig));
+                    }
+                }
+                else
+                {
+                    PCPP_LOG_ERROR("cannot open DPDK device - " << dpdkDev->getDeviceName() << ", link UP: " << linkSts.linkUp << ", Speed: " << linkSts.linkSpeedMbps << ", Duplex: " << linkSts.linkDuplex);
+                }
             }
             else
             {
-                PCPP_LOG_ERROR("Cannot find required DPDK device - " << dpdkDev->getDeviceName());
+                PCPP_LOG_ERROR("Cannot find required DPDK device - " << dev);
             }
         }
 
@@ -225,6 +305,20 @@ bool Dpdk_Ipv4::startProcess(const std::vector<std::string> devs, void (*callbac
 void Dpdk_Ipv4::stopProcess()
 {
     DpdkDeviceList::getInstance().stopDpdkWorkerThreads();
+}
+
+Dpdk_Dev_Rx_Stats* Dpdk_Ipv4::getDeviceStats(const std::string& dev)
+{
+    std::map<std::string, Dpdk_Dev_Rx_Stats*>::iterator search = stats.find(dev);
+
+    if (search != stats.end())
+    {
+        return search->second;
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
 } // namespace pcpp
